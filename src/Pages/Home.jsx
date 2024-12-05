2a1ac6bac07369f3502ba1b650fe17c14fd20335
karanwal-ha <karanwal-ha@ibjapan.jp>
2024-12-05 15:13:14 +0900

    12/05

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'; 
-import About from './About';
-import Order from './Order';
+import React, { useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom"; 
+import About from "./About";
+import Order from "./Order";
+import "./CSS/home.css";
+import arrow from "../Images/icons/thin-arrow.png";
 
 function Home() {
   const location = useLocation(); 
@@ -13,47 +15,50 @@ function Home() {
       const scrollPosition = window.scrollY;
 
       if (scrollPosition < aboutRef.current.offsetTop) {
-        if (location.pathname !== '/') {
-          window.history.replaceState(null, null, '/');
+        if (location.pathname !== "/") {
+          window.history.replaceState(null, null, "/");
         }
       } else if (
         scrollPosition >= aboutRef.current.offsetTop &&
         scrollPosition < orderRef.current.offsetTop
       ) {
-        if (location.pathname !== '/about') {
-          window.history.replaceState(null, null, '/about');
+        if (location.pathname !== "/about") {
+          window.history.replaceState(null, null, "/about");
         }
       } else {
-        if (location.pathname !== '/order') {
-          window.history.replaceState(null, null, '/order');
+        if (location.pathname !== "/order") {
+          window.history.replaceState(null, null, "/order");
         }
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [location]);
 
   return (
     <div>
+      
       <div className="backgroundHome" ref={aboutRef} >
-        <h1 style={{ fontSize: '48px', fontWeight: 'bold' }}>Welcome to Entropy</h1>
-        <p style={{ fontSize: '20px' }}>
-          Discover delicious pizzas made with love.
+        <h1 style={{ fontSize: "70px", fontWeight: 900, fontFamily:"Obviously" }}>Entropy Pizza</h1>
+        <p style={{ fontSize: "20px", fontWeight: 500, fontFamily:"Obviously", marginTop:"-30px" }}>
+          Discover delicious pizzas made with love.<br />
+          With flavours you wouldnt have imagined.
         </p>
-        <p style={{ fontSize: '20px' }}>
-          Explore our menu and place your order now!
-        </p>
+        <button className="button-main">Explore our selection<img className="arrow-main" src={arrow} alt=""/></button>
       </div>
+
       <div className="backgroundAbout" id="about" ref={orderRef} >
         <About />
       </div>
+
       <div className="backgroundOrder" id="order" ref={orderRef} >
         <Order />
       </div>
+
     </div>
   );
 }
